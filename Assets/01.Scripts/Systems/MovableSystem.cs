9e34a7259a50c83d9f0f6deffa5044d69794551d
rrd0627 <rrd0627@gmail.com>
2022-08-17 15:55:39 +0900

    add wall

--- Assets/01.Scripts/Systems/MovableSystem.cs
+++ Assets/01.Scripts/Systems/MovableSystem.cs
@@ -4,24 +4,15 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
-
+using Unity.Physics;
 public partial class MovableSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        float deltaTime = Time.DeltaTime;
-
-        Entities.ForEach((ref Movable move, ref Translation translation, ref Rotation rotation) =>
+        Entities.ForEach((ref Movable move, ref PhysicsVelocity phyVel) =>
         {
-            // Implement the work to perform for each entity here.
-            // You should only access data that is local or that is a
-            // field on this job. Note that the 'rotation' parameter is
-            // marked as 'in', which means it cannot be modified,
-            // but allows this job to run in parallel with other jobs
-            // that want to read Rotation component data.
-            // For example,
-            translation.Value += move.Speed * move.Direction * deltaTime;
-            rotation.Value = math.mul(rotation.Value.value,quaternion.RotateY(move.Speed * deltaTime));
+            var step = move.Direction * move.Speed;
+            phyVel.Linear = step;
         }).Schedule();
     }
 }
